d168de289dd349b7299889f7bda9c9890bb263a8
mspil88 <matthewspillane88@gmail.com>
2021-11-20 10:34:41 +0000

    time validation popup

--- static/index.js
+++ static/index.js
@@ -1,12 +1,23 @@
-var next1Btn = document.getElementById("next1-btn");
+const next1Btn = document.getElementById("next1-btn");
+const bedTime = document.getElementById("bed-time");
+const bedMinute = document.getElementById("bed-minute");
+const bedPart = document.getElementById("bed-part");
+const outTime = document.getElementById("out-time");
+const outMinute = document.getElementById("out-minute");
+const outPart = document.getElementById("out-part");
+const inputBackground = document.getElementById("input-background");
+const validationContainer = document.getElementById("gap-input-add");
+const allBtns = document.querySelectorAll("button");
 
-var bedTime = document.getElementById("bed-time");
-var bedMinute = document.getElementById("bed-minute");
-var bedPart = document.getElementById("bed-part");
+function toggleBackground() {
+    inputBackground.classList.toggle("visible");
+}
 
-var outTime = document.getElementById("out-time");
-var outMinute = document.getElementById("out-minute");
-var outPart = document.getElementById("out-part");
+function toggleValidationContainer() {
+    validationContainer.classList.toggle("visible");
+}
+
+
 
 console.log("test index.js");
 console.log(`next button ${next1Btn}`);
@@ -21,6 +32,12 @@ function bedWakeObj(bt, bm, bp, ot, om, op) {
 let defaultInBed = "Hour I went to bed:Minute I went to bedam/ pm";
 let defaultOutBed =  "Hour I got out of bed:Minute I got out of bedam/ pm";
 
+function validateInputs(bedWakeData, defaultInBed, defaultOutBed) {
+    if((bedWakeData.inbed === defaultInbed) || (bedWakeData.outbed === defaultOutbed)) {
+        return true;
+    }
+}
+
 next1Btn.addEventListener("click", () => {
     console.log("next btn clicked");
 
@@ -33,7 +50,9 @@ next1Btn.addEventListener("click", () => {
     console.log("bed wake data");
 
     if((bedWakeData.inbed === defaultInBed) || (bedWakeData.outbed === defaultOutBed)) {
-        console.log("invalid inputs");
+        //alert("invalid inputs");
+        toggleBackground();
+        toggleValidationContainer();
     } else {
         console.log(JSON.stringify(bedWakeData));
         $.ajax({
@@ -45,4 +64,35 @@ next1Btn.addEventListener("click", () => {
             .done(function(result) {console.log("success")})
         location.href = "/time_taken";
     }
+})
+
+allBtns.forEach(btn => {
+    btn.addEventListener("click", event => {
+        const action = event.currentTarget.id;
+        let bedWakeData = bedWakeObj(bedTime.value, bedMinute.value, bedPart.value,
+                                        outTime.value, outMinute.value, outPart.value);
+
+        if((action == "next1-btn") && validateInputs(bedWakeData, defaultInBed, defaultOutBed)) {
+            toggleBackGround();
+            toggleValidationContainer();
+
+        } else if((action == "next1-btn")) {
+            console.log(JSON.stringify(bedWakeData));
+            $.ajax({
+              url: Flask.url_for('index', {}),
+    //        url: '/index',
+             type: "POST",
+             data: JSON.stringify(bedWakeData),
+             contentType: "application/json"})
+            .done(function(result) {console.log("success")})
+        location.href = "/time_taken";
+        }
+    })
+})
+
+
+inputBackground.addEventListener("click", () => {
+    toggleBackground();
+    toggleValidationContainer();
+
 })
